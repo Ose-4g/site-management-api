887aace3c15e6bb3c72ee3fd1502c08131956870
Osemudiamen Itua <osemudiamenitua@gmail.com>
2023-04-18 15:53:00 +0100

    test: add test for the fetch managers and sites by companies

--- test/integration/company/fetch.manager.sites.spec.ts
+++ test/integration/company/fetch.manager.sites.spec.ts
@@ -1,27 +1,29 @@
 import 'reflect-metadata';
 
-import { ICompany, IManager } from '../../../src/models';
+import { ICompany, IManager, ISite } from '../../../src/models';
 import { INotificationService, ISessionService } from '../../../src/services';
 import app, { container } from '../../../src/app';
 import chai, { expect } from 'chai';
 import { database, generateToken, newCreateCompanyDTO, newInviteManagerDTO } from '../../helpers';
+import { newManagerDTO, newSiteDTO } from '../../helpers/manager';
 
 import { Model } from 'mongoose';
 import { StatusCodes } from 'http-status-codes';
 import { TYPES } from '../../../src/di';
 import chaiHttp from 'chai-http';
-import { newManagerDTO } from '../../helpers/manager';
 import sinon from 'sinon';
 
 chai.use(chaiHttp);
 
-const URL = '/api/v1/company/managers';
+const URL = '/api/v1/company/sites';
 const Company = container.get<Model<ICompany>>(TYPES.Company);
 const Manager = container.get<Model<IManager>>(TYPES.Manager);
+const Site = container.get<Model<ISite>>(TYPES.Site);
+
 const sessionService = container.get<ISessionService>(TYPES.SessionService);
 const notificationService = container.get<INotificationService>(TYPES.NotificationService);
 
-describe.only(`POST ${URL}`, () => {
+describe.only(`POST ${URL}/:siteId`, () => {
   before(async () => {
     await database.connect();
   });
@@ -29,24 +31,30 @@ describe.only(`POST ${URL}`, () => {
   afterEach(async () => {
     await Company.deleteMany();
     await Manager.deleteMany();
+    await Site.deleteMany();
   });
 
   after(async () => {
     await database.disconnect();
   });
 
-  it('should fetch managers successfully', async () => {
+  it('should fetch manager sites successfully', async () => {
     const company = await Company.create(newCreateCompanyDTO());
 
-    const managers: IManager[] = [];
+    const [manager1, manager2] = await Manager.create([
+      newManagerDTO({ company: company._id.toString() }),
+      newManagerDTO({ company: company._id.toString() }),
+    ]);
 
     for (let i = 0; i < 10; i++) {
-      const newManager = await Manager.create(newManagerDTO({ company: company._id.toString() }));
-      managers.push(newManager);
+      await Site.create([
+        newSiteDTO({ manager: manager1._id.toString() }),
+        newSiteDTO({ manager: manager2._id.toString() }),
+      ]);
     }
 
     const token = await generateToken({ userType: 'Company', id: company._id.toString() }, sessionService);
-    const response = await chai.request(app).get(URL).set('Authorization', token);
+    const response = await chai.request(app).get(`${URL}/${manager1._id.toString()}`).set('Authorization', token);
 
     expect(response.status).to.be.eq(StatusCodes.OK);
     expect(response.body.data.length).to.be.eq(10);
